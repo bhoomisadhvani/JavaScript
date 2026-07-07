6a65c7e99af0895e22fe1d6ca4e671dc31dedd28
bhoomisadhvani <bhoomisadhvani@gmail.com>
2026-07-07 10:03:28 +0530

    Add files via upload

---
+++ 01_add_js_in_html/script.js
@@ -0,0 +1,9 @@
+
+document.getElementById("heading")
+
+    document.getElementById("btn")
+        .addEventListener("click", function myFunction(){
+
+    document.getElementById("heading").innerHTML="welcome";
+
+        });
---
+++ 02_intro_and_concept/var.js
@@ -0,0 +1,19 @@
+
+
+var game="hide-seek";
+
+function playgames(){
+
+    var play="badminton";
+
+        console.log("game = - var.js:9", play);
+
+  if(true){
+
+  console.log("game = - var.js:13", play);
+
+
+  }
+}
+
+playgames();
