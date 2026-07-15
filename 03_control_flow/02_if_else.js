e76d71730ebb777b99c0653c0feab188052eb805
bhoomisadhvani <bhoomisadhvani@gmail.com>
2026-07-15 10:14:48 +0530

    conditional code added

---
+++ 03_control_flow/02_if_else.js
@@ -0,0 +1,28 @@
+// when the condition is false then the else block of code will be execute
+
+// 1
+
+let number = 7;
+if (number % 2 == 0) {
+  console.log("The number is even - 02_if_else.js:7");
+} else {
+  console.log("The number is odd - 02_if_else.js:9");
+}
+
+// 2
+
+let Raining = false;
+if (Raining) {
+  console.log("Take an umbrella - 02_if_else.js:16");
+} else {
+  console.log("No need for an umbrella - 02_if_else.js:18");
+}
+
+// 3
+
+let balance = true;
+if (balance) {
+  console.log("Withdrawal successful - 02_if_else.js:25");
+} else {
+  console.log("Insufficient balance - 02_if_else.js:27");
+}
