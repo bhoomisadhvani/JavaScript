13cfe6e2bc26880cc1e605b55c7cb622ba7aca06
bhoomi sadhvani <bhoomisadhvani@gmail.com>
2026-07-22 09:34:04 +0530

    code execution code added

---
+++ 05_code_execution/01_code_execution.js
@@ -0,0 +1,36 @@
+// var n = 2;
+
+// function square(num) {
+//   var result = num * num;
+
+//   return result;
+// }
+
+// square2 = square(n);
+
+// square4 = square(4);
+
+// console.log(square2);
+
+// console.log(square4);
+
+// 2
+
+var x = 1000;
+
+a();
+b();
+
+console.log(x);
+
+function a() {
+  var x = 10;
+
+  console.log(x);
+}
+
+function b() {
+  var x = 100;
+
+  console.log(x);
+}
